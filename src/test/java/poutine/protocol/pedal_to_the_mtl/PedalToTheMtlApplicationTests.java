7468033ebdb85451906574cb8ea215479d79875e
Darcy <darcymccoy4@gmail.com>
2025-10-01 11:52:19 -0400

    Add initial springboot setup

---
+++ src/test/java/poutine/protocol/pedal_to_the_mtl/PedalToTheMtlApplicationTests.java
@@ -0,0 +1,13 @@
+package poutine.protocol.pedal_to_the_mtl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedalToTheMtlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
