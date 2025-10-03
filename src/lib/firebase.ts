39346539bf34db3fcc3b22f0fc74c5fe0b4693b5
Priyesh Patel <priyeshpatel1216@gmail.com>
2025-10-03 19:15:35 -0400

    File and folder organization for login and register pages. Firebase application credentials added.

---
+++ src/lib/firebase.ts
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: process.env.NEXT_FIREBASE_API_KEY,
+    authDomain: "pedal-to-the-mtl.firebaseapp.com",
+    projectId: "pedal-to-the-mtl",
+    storageBucket: "pedal-to-the-mtl.firebasestorage.app",
+    messagingSenderId: "625823493809",
+    appId: "1:625823493809:web:87cfbfcd0e8d6e5e5b3c90",
+    measurementId: "G-TXFN76875L"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export default app;
