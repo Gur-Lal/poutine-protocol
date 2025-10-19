993b051ca923c26996104367a3e18a06bfc16ee8
Priyesh Patel <priyeshpatel1216@gmail.com>
2025-10-19 15:19:19 -0400

    Fix chain expression testing error

--- src/data/firebaseAdmin.ts
+++ src/data/firebaseAdmin.ts
@@ -1,13 +1,13 @@
 import { getApps, initializeApp, cert } from "firebase-admin/app";
-import {getFirestore} from "firebase-admin/firestore";
+import { getFirestore } from "firebase-admin/firestore";
 
 const projectId = process.env.FIREBASE_PROJECT_ID!;
 const clientEmail = process.env.FIREBASE_CLIENT_EMAIL!;
-const privateKey = process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n")!;
+const privateKey = process.env.FIREBASE_PRIVATE_KEY ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n") : "";
 
-if(!getApps().length){
+if (!getApps().length) {
     initializeApp({
-        credential: cert({projectId, clientEmail, privateKey}),
+        credential: cert({ projectId, clientEmail, privateKey }),
         projectId,
     });
 }
