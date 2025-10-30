a2bc157127ebb965a21852372d2607eb1ff8d367
Darcy <darcymccoy4@gmail.com>
2025-10-30 15:23:09 -0400

    Fix notifications

--- src/domain/services/notificationService.ts
+++ src/domain/services/notificationService.ts
@@ -1,12 +1,12 @@
 import { db } from "@/data/firebase";
 import { collection, addDoc, serverTimestamp } from "firebase/firestore";
 
-export async function createNotification(username: string, title: string, message: string) {
+export async function createNotification(email: string, title: string, message: string) {
     try {
         const notificationsRef = collection(db, "notifications");
 
         await addDoc(notificationsRef, {
-            username: username,
+            email: email,
             title: title,
             message: message,
             date: serverTimestamp(),
