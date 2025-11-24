ff6f645ab2e370dd1e753fe1a8bbfb4a289cc71a
Matthew Lucas Santiago <matthewlucassantiago04@gmail.com>
2025-11-24 17:35:28 -0500

    fix double attribute in pricebreakdown

--- src/domain/models/PriceBreakdown.ts
+++ src/domain/models/PriceBreakdown.ts
@@ -6,8 +6,6 @@ export interface PriceBreakdown {
   tierDiscount: number;
   dualRoleDiscount: number;
   total: number;
-  dualRoleDiscount?: number;
-  tierDiscount?: number;
   flexBalanceUsed?: number;
   flexBalanceEarned?: number;
 }
